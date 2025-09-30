6da3ef2a9a4f20ca4933d8f040b0dbff9ab07aca
Luchito333 <76989444@continental.edu.pe>
2025-09-30 16:33:23 -0500

    Despligue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "mongodb://mongo:wirKNrCHWRhAzrDfqGPdoWkMXJTxEmby@mongodb.railway.internal:27017",
 });
 
 export default api;
