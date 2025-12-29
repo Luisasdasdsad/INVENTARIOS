4831d030439e2a6b05a071dfd0c69380da5738af
Luchito333 <76989444@continental.edu.pe>
2025-12-29 12:33:00 -0500

    correo

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -15,8 +15,8 @@ export const enviarCorreo = async ({ to, subject, html }) => {
 
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 587,
-            secure: false, // false es requerido para el puerto 587 (STARTTLS)
+            port: 465,
+            secure: true, // true es requerido para el puerto 465 (SSL)
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
